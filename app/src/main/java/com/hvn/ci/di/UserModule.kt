fe4646c3a25fdc994a7c39cf57ff4dcdba26ce24
JefferssonSemin <jeffesemin@hotmail.com>
2021-01-05 11:24:21 -0300

    chore: adicionado camada di

---
+++ app/src/main/java/com/hvn/ci/di/UserModule.kt
@@ -0,0 +1,18 @@
+package com.hvn.ci.di
+
+import com.hvn.ci.data.remote.UserRemoteSource
+import com.hvn.ci.data.remote.UserRemoteSourceImpl
+import com.hvn.ci.data.repositories.UserRepositoryImpl
+import com.hvn.ci.domain.repositories.UserRepository
+import dagger.Binds
+import dagger.Module
+
+@Module
+abstract class UserModule {
+
+    @Binds
+    abstract fun providerUserRepository(userRepositoryImpl: UserRepositoryImpl): UserRepository
+
+    @Binds
+    abstract fun providerUserRemoteSource(userRemoteSourceImpl: UserRemoteSourceImpl): UserRemoteSource
+}

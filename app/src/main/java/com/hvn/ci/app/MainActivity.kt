506606519cdcfe040883d0c6d5bdf875884faa42
JefferssonSemin <jeffesemin@hotmail.com>
2021-01-11 16:10:55 -0300

    chore: ajustado alguns detalhes

--- app/src/main/java/com/hvn/ci/app/MainActivity.kt
+++ app/src/main/java/com/hvn/ci/app/MainActivity.kt
@@ -2,27 +2,19 @@ package com.hvn.ci.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.hvn.ci.R
 import com.hvn.ci.ui.UsuarioFragment
-import dagger.android.AndroidInjection
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.support.HasSupportFragmentInjector
-import javax.inject.Inject
+import dagger.hilt.android.AndroidEntryPoint
 
 
-abstract class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
 
     private val navController: NavController by lazy { findNavController(R.id.fragment_nav_host) }
 
-    @Inject
-    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -31,8 +23,6 @@ abstract class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
                 R.id.usuarioFragment -> UsuarioFragment()
             }
         }
+
     }
-
-    override fun supportFragmentInjector() = dispatchingAndroidInjector
-
 }

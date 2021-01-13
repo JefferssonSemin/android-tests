dc7530a7739da5bec306ac436809dc8d680a8af4
JefferssonSemin <jeffesemin@hotmail.com>
2021-01-13 15:47:13 -0300

    chore: tests parametrizados

--- app/src/test/java/com/hvn/ci/UsuarioAction.kt
+++ app/src/test/java/com/hvn/ci/UsuarioAction.kt
@@ -1,23 +1,44 @@
 package com.hvn.ci
 
 import com.hvn.ci.utils.validaUsuario
-import junit.framework.Assert.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 
-class UsuarioAction {
+@RunWith(Parameterized::class)
+class UsuarioAction(
+    private val esperado: Boolean,
+    private val nomeCerto: String,
+    private val nomeErrado: String
+) {
+    companion object {
+
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data() = listOf(
+            arrayOf(true, "Joao", "Rick"),
+            arrayOf(true, "Jeffersson", "Anderson"),
+            arrayOf(true, "Zeta", "Eliton")
+        )
+    }
 
     @Test
-    fun teste_buttonLimpar() {
-
-        // arrange
-        val esperado = true
-
+    fun test_validaUsuario() {
         // act
-        val atual = validaUsuario("Joao")
-
+        val atual = validaUsuario(nomeCerto)
 
         // assert
         assertEquals(esperado, atual)
     }
 
+    @Test
+    fun test_naoValidaUsuario() {
+        // act
+        val atual = validaUsuario(nomeErrado)
+
+        // assert
+        assertNotEquals(esperado, atual)
+    }
 }

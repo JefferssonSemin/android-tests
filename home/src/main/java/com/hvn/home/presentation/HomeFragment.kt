230bf9f2d757a80f36c8213e83a7c10a49411afa
JefferssonSemin <jeffesemin@hotmail.com>
2021-01-14 14:33:55 -0300

    chore: finalizado funcionalidades de navegacao

--- home/src/main/java/com/hvn/home/presentation/HomeFragment.kt
+++ home/src/main/java/com/hvn/home/presentation/HomeFragment.kt
@@ -1,32 +1,21 @@
 package com.hvn.home.presentation
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.hvn.home.R
+import com.hvn.home.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
-
-    private lateinit var viewModel: HomeViewModel
+    private lateinit var binding: HomeFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
+    ): View {
+        binding = HomeFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }

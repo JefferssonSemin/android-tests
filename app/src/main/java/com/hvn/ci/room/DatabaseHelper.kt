addbb78051b41781eed97be978ab2c287986a0f4
JefferssonSemin <jeffesemin@hotmail.com>
2021-01-20 14:34:34 -0300

    chore: criando helpers de database

---
+++ app/src/main/java/com/hvn/ci/room/DatabaseHelper.kt
@@ -0,0 +1,35 @@
+package com.hvn.ci.room
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.hvn.usuario.data.room.UsuarioDbContract
+
+class DatabaseHelper(context: Context?) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(UsuarioDbContract.SQL_CREATE_ENTRIES)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL(UsuarioDbContract.SQL_DROP_TABLE)
+        onCreate(db)
+    }
+
+    // Realiza o downgrade da versão do banco
+    override fun onDowngrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        super.onDowngrade(db, oldVersion, newVersion)
+    }
+
+    // Logo após o downgrade, é chamado o open para novas execuções
+    override fun onOpen(db: SQLiteDatabase?) {
+        super.onOpen(db)
+    }
+
+    companion object {
+        const val DATABASE_NAME = "usuario.db"
+        const val DATABASE_VERSION = 1
+    }
+}
--- usuario/src/main/java/com/hvn/usuario/domain/entities/Usuario.kt
+++ usuario/src/main/java/com/hvn/usuario/domain/entities/Usuario.kt
@@ -1,7 +1,8 @@
 package com.hvn.usuario.domain.entities
 
 class Usuario(
+    val id: Long,
     val name: String,
-    val url: String,
-    val avatar_url: String
+    val localization: String,
+    val url: String
 )
